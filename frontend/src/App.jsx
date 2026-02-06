d6674938c9541688df0aa57814e9efbdcf1e2101
Anteneh Wondwosen <antenehwondwosen@gmail.com>
2026-02-06 06:45:51 +0300

    adjust vertical overflow in mobile view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 // Updated App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
-import Home from './pages/Home';
-import About from './pages/About';
-import Support from './pages/Support';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import Community from './pages/Community';
-import CreateTask from './pages/CreateTask';
-import CreatePost from './pages/CreatePost';
-import TaskDetail from './pages/TaskDetail';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsOfService from './pages/TermsOfService';
-import Footer from './components/Footer';
-import './styles/global.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Support from "./pages/Support";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Community from "./pages/Community";
+import CreateTask from "./pages/CreateTask";
+import CreatePost from "./pages/CreatePost";
+import TaskDetail from "./pages/TaskDetail";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
+import Footer from "./components/Footer";
+import "./styles/global.css";
 
 function App() {
   return (
@@ -31,34 +36,49 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/privacy" element={<PrivacyPolicy />} />
             <Route path="/terms" element={<TermsOfService />} />
-            
+
             {/* Protected Routes */}
-            <Route path="/dashboard" element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            } />
-            <Route path="/community" element={
-              <PrivateRoute>
-                <Community />
-              </PrivateRoute>
-            } />
-            <Route path="/create-task" element={
-              <PrivateRoute>
-                <CreateTask />
-              </PrivateRoute>
-            } />
-            <Route path="/create-post" element={
-              <PrivateRoute>
-                <CreatePost />
-              </PrivateRoute>
-            } />
-            <Route path="/tasks/:id" element={
-              <PrivateRoute>
-                <TaskDetail />
-              </PrivateRoute>
-            } />
-            
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/community"
+              element={
+                <PrivateRoute>
+                  <Community />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/create-task"
+              element={
+                <PrivateRoute>
+                  <CreateTask />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/create-post"
+              element={
+                <PrivateRoute>
+                  <CreatePost />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tasks/:id"
+              element={
+                <PrivateRoute>
+                  <TaskDetail />
+                </PrivateRoute>
+              }
+            />
+
             {/* Redirect any unknown route to home */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
