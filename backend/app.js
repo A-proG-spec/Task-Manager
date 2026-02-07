85818dccdaf9b4ac3244b5dad7702f4e0c6a5be3
Anteneh Wondwosen <antenehwondwosen@gmail.com>
2026-02-08 01:56:17 +0300

    adjust cors configuration of the backed

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,31 @@ import authRoutes from "./routes/authRotes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import postRouters from "./routes/PostRoutes.js";
-import cors from "cors"
-
+import cors from "cors";
 
 dotenv.config();
 const app = express();
-const allowedOrigins = process.env.FRONTEND_URL
+
 app.use(express.json());
-app.use(cors({
-  origin:allowedOrigins,
-  credentials: true
-}))
+
+const allowedOrigins = [
+  "https://task-manager-five-alpha-56.vercel.app",
+  "https://task-manager-git-master-antenehs-projects-69af1458.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl) or check against whitelist
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/admin", adminRoutes);
